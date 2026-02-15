cd69855769a418260524a80623d34ebd774cf889
Alireza Rashedi <nobody1381rashedi@gmail.com>
2026-02-16 00:09:00 +0330

    Add new source file for variable management implementation

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// this is a comment
+
+/*
+this is a multi-line comment
+*/
+int main() {
+    std::cout << "I like freedom!" << '\n';
+    std::cout <<"It is really good to be free!" << '\n';
+    return 0;
+}
