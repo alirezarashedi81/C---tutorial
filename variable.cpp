2a8ff8fa996b4243b01f1140573d8c7cbe04e847
Alireza Rashedi <nobody1381rashedi@gmail.com>
2026-02-16 00:35:39 +0330

    Refactor code structure for improved readability and maintainability

---
+++ variable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+/*
+int main(){
+    //declaring a variable
+    int x;
+    //assigning a value to the variable
+    x = 5; 
+    std::cout << x << '\n';
+
+    //doing both at the same time
+    int y = 10;
+    std::cout << y << '\n';
+
+    // using the variables to do some math
+    int sum = x + y;
+    std::cout << sum << '\n';
+}
+*/
+
+int main(){
+    int year = 2024;
+    int month = 6;
+    double day = 15.7;
+
+    std::cout << "Today is " << year << '-' << month << '-' << day << '\n';
+}
