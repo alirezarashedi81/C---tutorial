e1a26e2f960fbed31beb68a6757120d0a53c988a
Alireza Rashedi <nobody1381rashedi@gmail.com>
2026-02-16 14:26:30 +0330

    Add initial implementation of namespace and constant files
    - Created a new C++ source file for namespace handling in `.vscode/namespace.cpp`.
    - Added a binary file `constant` containing compiled data or resources.

--- constant.cpp
+++ constant.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+
+int main(){
+    const double pi = 3.14159;
+    const int SpeedOfLight = 299792458; 
+    const int windowWidth = 1920;
+    const int windowHeight = 1080;
+    return 0;
+  
+}
