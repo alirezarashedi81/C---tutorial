58b44617363f9c5bdb6b845ca301e5fdfcb08314
Alireza Rashedi <nobody1381rashedi@gmail.com>
2026-02-19 19:36:12 +0330

    Add typedefs for number and string types in new typedef.cpp file
    - Introduced `number_t` as an alias for `int`
    - Introduced `text_t` as an alias for `std::string`
    - Implemented a simple main function to demonstrate usage

---
+++ typedef.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using number_t = int;
+using text_t = std::string;
+
+int main(){
+    number_t age = 23;
+    text_t FirstName = "Alireza";
+
+    std::cout << "age: " << age << std::endl;
+    std::cout << "name: " << FirstName << std::endl;
+
+    return 0;
+}
